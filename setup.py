aa409cfef871bb1eb25aa40417620a1dfe020055
Appy-success <Appy_ms@outlook.com>
2025-12-30 14:45:24 +0530

    setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+from typing import list
+
+def get_requirements(file_path:str)->list[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+    with open(requirements.txt) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+setup(
+    name="ml_project",
+    version="0.1",
+    author="Apana Rajawat",
+    author_email="appy_ms@outlook.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+    )
